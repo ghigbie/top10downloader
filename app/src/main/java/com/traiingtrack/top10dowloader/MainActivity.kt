eb5bcf307efacb1c2839c9544ac38929a15b0a69
Derfel Cadarn <gehigbie@gmail.com>
2020-02-17 21:24:04 -0700

    Add new download method

--- app/src/main/java/com/traiingtrack/top10dowloader/MainActivity.kt
+++ app/src/main/java/com/traiingtrack/top10dowloader/MainActivity.kt
@@ -4,6 +4,12 @@ import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,13 +34,40 @@ class MainActivity : AppCompatActivity() {
                 Log.d(TAG, "onPostExecute called: paramete is $result")
             }
 
-            override fun doInBackground(vararg p0: String?): String {
-                Log.d(TAG, "doInBackground called: starts with ${p0[0]}")
-                return "doInBackground completed"
+            override fun doInBackground(vararg url: String?): String {
+                Log.d(TAG, "doInBackground called: starts with ${url[0]}")
+                val rssFeed = downloadXML(url[0])
+                if (rssFeed.isEmpty){
+                    Log.e(TAG, "doInBackground: Error downloading")
+                }
+                return rssFeed
             }
-
         }
     }
+
+    private fun downloadXML(urlPath: String?): String{
+        val xmlResult = StringBuilder()
+
+        try{
+            val url = URL(urlPath)
+            val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+            val response = connection.responseCode
+            Log.d(TAG, "downloadXML: The respose code was $response")
+
+            val inputStream = connection.inputStream
+            val inputStreamReader = InputStreamReader(inputStream)
+            val reader = BufferedReader(inputStreamReader)
+
+        }catch(e: MalformedURLException){
+            Log.e(TAG, "downloadXML: Invalid URL ${e.message}")
+        }catch(e: IOException){
+            Log.e(TAG, "downloadXML: IO Exception reading data ${e.message}")
+        }
+    }
+
+
+
+
 
 
 }
