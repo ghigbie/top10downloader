fda47811dd8df8d45ac0edcfc6861a91642bc07b
Derfel Cadarn <gehigbie@gmail.com>
2020-02-17 20:33:15 -0700

    Add methods

--- app/src/main/java/com/traiingtrack/top10dowloader/MainActivity.kt
+++ app/src/main/java/com/traiingtrack/top10dowloader/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.traiingtrack.top10dowloader
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity";
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "onCreate called")
+        val downloadData = DownloadData()
+        downloadData.execute("URL GOES HERE")
+        Log.d(TAG, "onCreate: done")
     }
+
+    companion object {
+        private class DownloadData : AsyncTask<String, Void, String>() {
+            private val TAG = "DownloadData"
+
+            override fun onPostExecute(result: String?) {
+                super.onPostExecute(result)
+                Log.d(TAG, "onPostExecute called: paramete is $result")
+            }
+
+            override fun doInBackground(vararg p0: String?): String {
+                Log.d(TAG, "doInBackground called: starts with ${p0[0]}")
+                return "doInBackground completed"
+            }
+
+        }
+    }
+
+
 }
