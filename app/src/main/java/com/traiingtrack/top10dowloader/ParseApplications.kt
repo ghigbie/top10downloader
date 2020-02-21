f4a3882eea357d10978027a468f7b492f35f01a8
Derfel Cadarn <gehigbie@gmail.com>
2020-02-20 21:00:58 -0700

    Edit class

--- app/src/main/java/com/traiingtrack/top10dowloader/ParseApplications.kt
+++ app/src/main/java/com/traiingtrack/top10dowloader/ParseApplications.kt
@@ -1,6 +1,9 @@
 package com.traiingtrack.top10dowloader
 
 import android.util.Log
+import org.xmlpull.v1.XmlPullParser
+import org.xmlpull.v1.XmlPullParserFactory
+import java.lang.Exception
 
 class ParseApplications {
     private val TAG = "parseApplication"
@@ -13,10 +16,48 @@ class ParseApplications {
         var textValue = ""
 
         try{
+            val factory = XmlPullParserFactory.newInstance()
+            factory.isNamespaceAware = true
+            val xpp = factory.newPullParser()
+            xpp.setInput(xmlData.reader())
+            var eventType = xpp.eventType
+            var currentRecord = FeedEntry()
+            while (eventType != XmlPullParser.END_DOCUMENT){
+                val tagName = xpp.name.toLowerCase()
+                when (eventType){
+                    XmlPullParser.START_TAG -> {
+                        Log.d(TAG, "parse: Starting: tag for $tagName")
+                        if (tagName == "entry"){
+                            inEntry = true
+                        }
+                    }
 
-        }catch{
+                    XmlPullParser.TEXT -> textValue = xpp.text
 
+                    XmlPullParser.END_TAG -> {
+                        Log.d(TAG, "parse: Ending tag for $tagName")
+                        if(inEntry){
+                            when(tagName) {
+                                "entry" -> {
+                                    applications.add(currentRecord)
+                                    inEntry = false
+                                    currentRecord = FeedEntry()
+                                }
+                                "name" -> currentRecord.name = textValue
+                                "artist" -> currentRecord.artist = textValue
+                                "releaseDate" -> currentRecord.releaseDate = textValue
+                                "summary" -> currentRecord.summary = textValue
+                                "image" -> currentRecord.imageURL = textValue
+                            }
+                        }
+                    }
+                }
+            }
+        }catch(e: Exception){
+            e.printStackTrace()
+            status = false
         }
+        return status
     }
 
 }
